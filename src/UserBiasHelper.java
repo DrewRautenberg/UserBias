b55478c956aa9ecd398b75322f7267b602ffc639
Drew Rautenberg <drew@drewrautenberg.com>
2017-10-04 15:18:37 -0400

    finished coding

--- src/UserBiasHelper.java
+++ src/UserBiasHelper.java
@@ -4,7 +4,18 @@ public class UserBiasHelper {
         out = numTries+num;
         return out;
     }
-    public static void printResults(int tries, int evens){
+    public static void printResults(int numTries, int numEvens){
+        double percent = numTries*.2;
+        int numOdds = numTries-numEvens;
+        int diff = Math.abs(numEvens-numOdds);
+        if (diff >= percent){
+            System.out.printf("%d tries: %d even, %d odd. BIASED", numTries, numEvens, numOdds);
+        }
+        else {
+            System.out.printf("%d tries: %d even, %d odd. NOT BIASED", numTries, numEvens, numOdds);
+        }
+
+
 
     }
 }
